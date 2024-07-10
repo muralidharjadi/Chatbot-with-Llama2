ff458311cc91afbb418d000a49d9d05ae4aae0c0
muralidharjadi <muralidhar.jadi@gmail.com>
2024-07-11 02:17:33 +0530

    llm tested

--- app.py
+++ app.py
@@ -4,15 +4,13 @@ from langchain_chroma import Chroma
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
+from langchain_openai import OpenAI
 from dotenv import load_dotenv
 from src.prompt import *
 import os
-import json
-import torch
-from transformers import (AutoTokenizer,
-                          AutoModelForCausalLM,
-                          BitsAndBytesConfig,
-                          pipeline)
+from dotenv import load_dotenv
+load_dotenv()
+os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
@@ -26,25 +24,6 @@ docs = db3.similarity_search(query)
 
 PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
-chain_type_kwargs={"prompt": PROMPT}
-config_data = json.load(open("config.json"))
-HF_TOKEN = config_data["HF_TOKEN"]
-model_name = "meta-llama/Meta-Llama-3-8B"
-
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-)
-tokenizer = AutoTokenizer.from_pretrained(model_name,
-                                          token=HF_TOKEN)
-
-tokenizer.pad_token = tokenizer.eos_token
-llm = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    device_map="auto",
-    quantization_config=bnb_config,
-    token=HF_TOKEN
-)
-
+llm = OpenAI(temperature=0.6)
+response = llm.invoke("What is Capital of India?")
+print(response)
