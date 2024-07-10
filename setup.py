d2ebf6a8a2927a912f5e89b5eb89fe1a5f5b2191
muralidharjadi <muralidhar.jadi@gmail.com>
2024-07-10 20:43:20 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot Llama3',
+    version= '0.0.0',
+    author= 'Muralidhar Jadi',
+    author_email= 'muralidhar.jadi@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
